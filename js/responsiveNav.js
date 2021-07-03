c4ac6589b32f9ad3329010817f43514a671d5f97
jlcareglio <jlcareglioalbornoz@gmail.com>
2021-07-03 20:18:40 -0300

    navBar vinculada a diferentes secciones

--- js/responsiveNav.js
+++
@@ -1,3 +0,0 @@
-/* Este script se encargara de agregar/quitar la clase 'active' en
-los elementos del 'nav' a medida que se entran/salen en/de
-las distintas secciones de la pagina o se pulsan enlaces en el nav */
