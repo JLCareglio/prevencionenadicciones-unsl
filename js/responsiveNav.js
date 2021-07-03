6d25a1da205036fc242185bafcb1fb3616622f6f
jlcareglio <jlcareglioalbornoz@gmail.com>
2021-07-03 01:05:08 -0300

    responsiveNav v0.7

---
+++ js/responsiveNav.js
@@ -0,0 +1,3 @@
+/* Este script se encargara de agregar/quitar la clase 'active' en
+los elementos del 'nav' a medida que se entran/salen en/de
+las distintas secciones de la pagina o se pulsan enlaces en el nav */
