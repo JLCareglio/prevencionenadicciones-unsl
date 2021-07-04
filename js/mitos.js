c57073d4de9a72738e2a2364b9dd27eaf0755e40
marianoramirez123 <64232904+marianoramirez123@users.noreply.github.com>
2021-07-04 04:30:59 -0300

    Add files via upload

---
+++ js/mitos.js
@@ -0,0 +1,12 @@
+var mito1=0
+
+function mostrar() {
+if (mito1==0) {
+mito1=1;
+ document.getElementById("mito1").style.display = "block";
+}else{
+mito1=0
+ document.getElementById("mito1").style.display = "none";	
+}
+	
+}
