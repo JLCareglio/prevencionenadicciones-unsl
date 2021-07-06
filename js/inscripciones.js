d8b6e381bb0be709f48c67e65ba650e3f5904744
jlcareglio <jlcareglioalbornoz@gmail.com>
2021-07-05 23:33:34 -0300

    validaciones en inscripciones v0.7

--- js/inscripciones.js
+++ js/inscripciones.js
@@ -1,3 +1,12 @@
+// Variables de todos los inputs del formulario de inscripcion
+var inputEmail = document.getElementById("emailAddress");
+var inputNombre = document.getElementById("7835639");
+var inputEvento = bootstrapForm.elements["entry.1134359719"];
+var inputComoEnteraste = bootstrapForm.elements["entry.253668503"];
+var inputComoEnterasteOtraOpcion =
+  bootstrapForm.elements["entry.253668503.other_option_response"];
+var inputComentarios = document.getElementById("710177639");
+
 /* ----------Actualiza listado de eventos para inscripciones---------- */
 // Esta es la referencia a la Hoja de Calculo de Google externa que tiene los eventos con sus fechas y horarios
 const hojaEventos =
@@ -9,6 +18,7 @@ const hojaNovedades = SPREADSHEET + "o7d3cgn/public/values?alt=json";
 const hojaEncuestas = SPREADSHEET + "o57dwj3/public/values?alt=json";
 */
 
+fetchInscripciones();
 async function fetchInscripciones() {
   const response = await fetch(hojaEventos);
   let json_eventos = await response.json();
@@ -17,12 +27,49 @@ async function fetchInscripciones() {
 }
 
 /* ----------Validar Formulario---------- */
-// Variables de todos los inputs a validar
-var inputEmail = document.getElementById("emailAddress");
-var inputNombre = document.getElementById("7835639");
-var inputEvento = document.getElementById("831586866");
 // Asignacion de funciones que validan lo ingresado
 //inputEmail.addEventListener("focusout", validarEmail);
+inputEmail.addEventListener("focusout", validarEmail);
+inputNombre.addEventListener("focusout", validarNombre);
+inputComoEnteraste[3].addEventListener("click", validarOtraOpcion);
+inputComoEnterasteOtraOpcion.addEventListener("focusout", validarOtraOpcion);
+inputComoEnterasteOtraOpcion.addEventListener("focus", function () {
+  inputComoEnteraste[3].checked = true;
+});
+
+function validarEmail() {
+  const exprecionRegular =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (exprecionRegular.test(String(inputEmail.value).toLowerCase())) {
+    console.log("email valido");
+    return true;
+  } else {
+    console.log("email invalido");
+    return false;
+  }
+}
+function validarNombre() {
+  const exprecionRegular = /^[A-Za-z\s]+$/;
+  if (exprecionRegular.test(String(inputNombre.value).toLowerCase())) {
+    console.log("nombre valido");
+    return true;
+  } else {
+    console.log("nombre invalido");
+    return false;
+  }
+}
+function validarOtraOpcion() {
+  if (
+    inputComoEnteraste.value == "__other_option__" &&
+    inputComoEnterasteOtraOpcion.value == ""
+  ) {
+    console.log("por favor, escribe algo si seleccionas 'otra opción'");
+    return false;
+  } else {
+    console.log("'otra opción' ok");
+    return true;
+  }
+}
 
 /* ----------Envio de inscripcion a GoogleForm---------- */
 // This script requires jQuery and jquery-form plugin
