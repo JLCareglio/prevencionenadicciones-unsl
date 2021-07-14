3f634f0f1c6e347d0da6207a2bc18a6b4c5a98e5
jlcareglio <jlcareglioalbornoz@gmail.com>
2021-07-14 18:51:34 -0300

    test para hacer funcional la seccion contacto

---
+++ docs/js/contacto.js
@@ -0,0 +1,17 @@
+/* ----------Envio de consulta a GoogleForm---------- */
+// This script requires jQuery and jquery-form plugin
+$("#bootstrapForm").submit(function (event) {
+  event.preventDefault();
+  var extraData = {};
+  $("#bootstrapForm").ajaxSubmit({
+    data: extraData,
+    dataType: "jsonp", // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
+    error: function () {
+      // Submit of form should be successful but JSONP callback will fail because Google Forms
+      // does not support it, so this is handled as a failure.
+      alert("Form Submitted. Thanks.");
+      // You can also redirect the user to a custom thank-you page:
+      // window.location = 'http://www.mydomain.com/thankyoupage.html'
+    },
+  });
+});
